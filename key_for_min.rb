cd898ef0b9b41647198ffd9551d476e920a0a1d8
Corina Talavera <corina.talavera@gmail.com>
2018-10-29 23:20:56 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-cat_food = { :raw => 2
+cat_food = { "raw"=> "7", "can" => "3", 
 def key_for_min_value(name_hash)
 
 end
